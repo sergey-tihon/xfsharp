abf7f17d6114290c7a8ccaccaf7e80e1180fcc47
Jon <jwood803@users.noreply.github.com>
2014-06-04 16:46:46 -0400

    Initial add of grade school exercise

---
+++ grade-school/Example.fs
@@ -0,0 +1,18 @@
+﻿module School
+
+type School() =
+    member val Roster = Map.empty with get, set
+
+    member this.add(grade: int, studentName: string) =
+        let studentsOpt = this.Roster.TryFind(grade) 
+        // If the result was 'None', then use empty list as the default 
+        let students = defaultArg studentsOpt []
+        // Create a new list with the new student at the front
+        let newStudents = studentName::students
+        // Create & save map with new/replaced mapping for 'grade'
+        this.Roster <- this.Roster.Add(grade, newStudents)
+
+    member this.grade(grade: int) =
+        match this.Roster.TryFind(grade) with
+            | Some students -> students |> List.sort
+            | None -> List.empty
---
+++ grade-school/GradeSchoolTest.fs
@@ -0,0 +1,70 @@
+﻿module GradeSchoolTest
+
+open NUnit.Framework
+open School
+
+[<TestFixture>]
+type GradeSchoolTest() =
+
+    [<Test>]
+    member tests.New_school_has_an_empty_roster() =
+        let grades = School()
+
+        Assert.That(grades.Roster, Has.Count.EqualTo(0))
+
+    [<Test>]
+    member tests.Adding_a_student_adds_them_to_the_roster_for_the_given_grade() =
+        let grades = School()
+
+        grades.add(2, "Aimee")
+
+        Assert.That(grades.Roster.[2], Is.EqualTo(["Aimee"]))
+
+    [<Test>]
+    member tests.Adding_more_students_to_the_same_grade_adds_them_to_the_roster() =
+        let grades = School()
+
+        grades.add(2, "Blair")
+        grades.add(2, "James")
+        grades.add(2, "Paul")
+
+        Assert.That(grades.Roster.[2], Is.EqualTo(["Blair"; "James"; "Paul";]))
+
+    [<Test>]
+    member tests.Adding_students_to_different_grades_adds_them_to_the_roster() =
+        let grades = School()
+
+        grades.add(3, "Chelsea")
+        grades.add(7, "Logan")
+
+        Assert.That(grades.Roster.[3], Is.EqualTo(["Chelsea"]))
+        Assert.That(grades.Roster.[7], Is.EqualTo(["Logan"]))
+
+    [<Test>]
+    member tests.Grade_returns_the_students_in_that_grade_in_alphabetical_order() =
+        let grades = School()
+
+        grades.add(5, "Franklin")
+        grades.add(5, "Bradley")
+        grades.add(1, "Jeff")
+
+        Assert.That(grades.grade(5), Is.EqualTo(["Bradley"; "Franklin"]))
+
+    [<Test>]
+    member tests.Grade_returns_an_empty_list_if_there_are_no_students_in_that_grade() =
+        let grades = School()
+
+        Assert.That(grades.grade(1), Is.EqualTo([]))
+
+    [<Test>]
+    member tests.Student_names_in_each_grade_in_roster_are_sorted() =
+        let grades = School()
+
+        grades.add(4, "Jennifer");
+        grades.add(6, "Kareem");
+        grades.add(4, "Christopher");
+        grades.add(3, "Kyle");
+
+        Assert.That(grades.Roster.[3], Is.EqualTo(["Kyle"]))
+        Assert.That(grades.Roster.[4], Is.EqualTo(["Christopher"; "Jennifer"]))
+        Assert.That(grades.Roster.[6], Is.EqualTo(["Kareem"]))
