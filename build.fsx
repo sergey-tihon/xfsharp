3675db9617c2b06ebb804c95ef0b7fce65e92103
Jon <jwood803@users.noreply.github.com>
2014-09-11 13:47:43 -0400

    Build file update

--- build.fsx
+++ build.fsx
@@ -3,13 +3,11 @@ open System.IO
 open Fake
 open Fake.FileSystemHelper
 open Fake.FscHelper
-let allDirs = DirectoryInfo(__SOURCE_DIRECTORY__).GetDirectories "*"
-let currentDir = DirectoryInfo(__SOURCE_DIRECTORY__ + "\\" + allDirs.[2].Name)
+let allDirs = 
+    DirectoryInfo(__SOURCE_DIRECTORY__).GetDirectories "*"
+        |> Array.collect(fun d -> filesInDirMatching "Example.fs" d)
 
-let exampleFiles = 
-    filesInDirMatching "Example.fs" currentDir
-        |> Array.map(fun i -> i.FullName)
-        |> Array.toList
+let exampleFiles = allDirs |> Array.map(fun i -> i.FullName) |> Array.toList
 
 Target "Default" (fun _ ->
     exampleFiles |> Fsc(fun param -> { param with FscTarget = Library
