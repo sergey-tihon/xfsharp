60b7a917fe4cd667990d83925f623c7d89402b4f
Jon <jwood803@users.noreply.github.com>
2014-09-11 10:30:59 -0400

    Compile the Example files

--- build.fsx
+++ build.fsx
@@ -1,10 +1,21 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
+open System.IO
 open Fake
+open Fake.FileSystemHelper
+open Fake.FscHelper
 
-// Default target
+let nunitDll = "packages\NUnit.Runners\tools\lib\nunit.core.dll"
+let allDirs = DirectoryInfo(__SOURCE_DIRECTORY__).GetDirectories "*"
+let currentDir = DirectoryInfo(__SOURCE_DIRECTORY__ + "\\" + allDirs.[2].Name)
+
+let exampleFiles = 
+    filesInDirMatching "Example.fs" currentDir
+        |> Array.map(fun i -> i.FullName)
+        |> Array.toList
+
 Target "Default" (fun _ ->
-    trace "Hello World from FAKE"
+    exampleFiles |> Fsc(fun param -> { param with References = [nunitDll]
+                                                  FscTarget = Library })
 )
 
-// start build
 RunTargetOrDefault "Default"
